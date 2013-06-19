6c5a2e4b9708f2948d7213eae2fd9708afffcc5a
osmltir <ldelevallez@gmail.com>
2013-06-19 15:55:13 +0200

    Second import

---
+++ index.php
@@ -0,0 +1,151 @@
+<!--
+To change this template, choose Tools | Templates
+and open the template in the editor.
+-->
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Actualités Tir Sportif</title>
+        <link rel="icon" href="bootstrap/img/favicon.ico" type="image/ico" />
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <!-- On ouvre la fenêtre à  la largeur de l'écran -->
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <!-- Intégration du CSS Bootstrap -->
+        <link href="bootstrap/css/bootstrap.css" rel="stylesheet" media="screen">
+        <!-- Intégration du CSS responsive Bootstrap -->
+        <link href="bootstrap/css/bootstrap-responsive.min.css" rel="stylesheet">
+    </head>
+  
+    <body>
+        <!-- Conteneur principal -->
+        <div class="container">
+		
+            <!-- Barre de navigation -->
+            <?php include("nav.php"); ?>
+			
+                <!-- Première ligne de notre grille -->
+                    <div class="row">
+                        <div class="span7">
+                            <div id="myCarousel" class="carousel slide">
+                                <!-- Carousel items -->
+                                    <div class="carousel-inner">
+                                        <div class="active item">
+                                            <img src="http://twitter.github.com/bootstrap/assets/img/bootstrap-mdo-sfmoma-01.jpg" alt="">
+                                            <div class="carousel-caption">
+                                                <h4>1er slide</h4>
+                                                <p>
+                                                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut
+                                                labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris ...
+                                                </p>
+                                            </div>
+                                        </div>
+                                        <div class="item">
+                                            <img src="http://twitter.github.com/bootstrap/assets/img/bootstrap-mdo-sfmoma-02.jpg" alt="">
+                                            <div class="carousel-caption">
+                                                <h4>2eme slide</h4>
+                                                <p>
+                                                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut
+                                                labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris ...
+                                                </p>
+                                            </div>
+                                        </div>
+                                        <div class="item">
+                                            <img src="http://twitter.github.com/bootstrap/assets/img/bootstrap-mdo-sfmoma-03.jpg" alt="">
+                                            <div class="carousel-caption">
+                                                <h4>3eme slide</h4>
+                                                <p>
+                                                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut 
+                                                labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris ...
+                                                </p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <!-- Carousel nav -->
+                                    <a class="carousel-control left" href="#myCarousel" data-slide="prev">&lsaquo;</a>
+                                    <a class="carousel-control right" href="#myCarousel" data-slide="next">&rsaquo;</a>
+                                </div>
+                            </div>
+				
+				<div class="span5 hidden-phone">
+					<a href="#" class="thumbnail">
+						<img src="bootstrap/img/sansalcool.png" class="img-rounded">
+					</a>
+				</div>
+                    </div>
+
+                <!-- Deuxième ligne de notre grille -->
+                    <div class="row">
+                        <div class="span4">
+                            <h2>De la graine de champions à l’OSML section Tir.</h2>le 26/06/2012<br/>
+                            <br/>
+                            <p>
+                            Depuis quelques semaines, les jeunes tireurs de l’OSM Lomme Section Tir sont à l’honneur.<br/>
+                            Début Juin le club a organisé la rencontre régionale été des écoles de tir poussins-benjamins (UFOLEP).Ce championnat a regroupé les grands espoirs
+                            des disciplines pistolet 10 mètres et carabine 10 mètres des clubs du Nord (Lomme, Bruay sur Escaut, Hazebrouck, Moncheaux,…) et du Pas de Calais
+                            (Béthune, Audruicq).
+                            </p>
+                            <p><a class="btn" href="grchampions.php">En savoir plus <i class="icon-chevron-right"></i></a></p>
+                        </div>
+                        <div class="span4">
+                            <h2>Paragraphe avec image</h2>
+                            <p>
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore
+                            et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ...
+                            <img src="http://placehold.it/360x100" class="img-rounded">
+                            </p>
+                            <p><a class="btn" href="#">En savoir plus <i class="icon-chevron-right"></i></a></p>
+                        </div>
+                        <div class="span4">
+                            <h2>Adresse</h2>
+                            <p>
+                            Le stand de tir se trouve au sous-sol de la piscine Municipal
+                            </p>
+                            <address>
+                                <strong>OSML Section Tir</strong><br>
+                                    433 Bis Avenue de Dunkerque<br>
+                                    59160 Lomme, France<br>
+                                    <abbr title="Phone">P:</abbr> (+33)621-754-703
+                            </address>
+                            <address>
+                                <strong>Full Name</strong><br>
+                                <a href="mailto:#">osmltir@gmail.com</a>
+                            </address>
+                            <p><a class="btn" href="#">En savoir plus <i class="icon-chevron-right"></i></a></p>
+                        </div>
+                    </div>
+
+                    <hr class="bs-docs-separator">
+		  
+                <!-- Troisième ligne de notre grille -->
+                    <div class="row">
+                        <div class="span12">
+                            <h1>Zone de texte</h1>
+                            <p>
+                            ed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi 
+                            architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione 
+                            voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet,  ...
+                            </p>
+                            <p>
+                            At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati 
+                            cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam 
+                            libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. 
+                            Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic 
+                            tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.
+                            </p>
+                        </div>
+                    </div>
+
+                        <hr class="bs-docs-separator">
+		  
+                <!-- Footer -->
+                    <?php include("footer.php"); ?>
+	  
+        </div>
+    
+        <!-- Intégration de la libraire jQuery -->
+        <script src="bootstrap/js/jquery-1.9.1.min.js"></script>
+        <!-- Intégration de la libraire de composants du Bootstrap -->
+        <script src="bootstrap/js/bootstrap.min.js"></script>
+		
+    </body>
+</html>
